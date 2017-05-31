2cdacade7abf37dad35ce95742efdb3a09547c35
Josie Elordi <josie.elordi@llamasoft.com>
2017-05-31 16:19:59 -0400

    Getting into Redux

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router';
 
 class HomePage extends React.Component {
-  render() new Promise(function(resolve, reject) {
+  render() {
     return (
       <div className="jumbotron">
         <h1>Pluralsight Administration</h1>
