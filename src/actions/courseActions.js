48f59164fb7fa89424df58af82b456db01f9e6fe
Josie Elordi <josie.elordi@llamasoft.com>
2017-06-06 15:43:38 -0400

    Update

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
   //debugger;
@@ -16,6 +17,7 @@ export function updateCourseSuccess(course) {
 
 export function loadCourses() {
   return function(dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
@@ -26,10 +28,12 @@ export function loadCourses() {
 
 export function saveCourse(course) {
   return function(dispatch, getState) {
-    return courseApi.saveCourse(course).then(savedCourse => {
-      course.id ? dispatch(updateCourseSuccess(saveCourse)) :
-        dispatch(createCourseSuccess(savedCourse));
+    dispatch(beginAjaxCall());
+    return courseApi.saveCourse(course).then(course => {
+      course.id ? dispatch(updateCourseSuccess(course)) :
+        dispatch(createCourseSuccess(course));
     }).catch(error => {
+      dispatch(ajaxCallError(error));
       throw(error);
     });
   };
